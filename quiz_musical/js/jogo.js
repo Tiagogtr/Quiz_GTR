5da79231d932d3e18383d9f72e25d893d0054655
Tiagogtr <71888086+Tiagogtr@users.noreply.github.com>
2021-01-21 18:08:04 -0300

    Add files via upload

---
+++ quiz_musical/js/jogo.js
@@ -0,0 +1,176 @@
+var pontoAtual = 1
+function pergunta1() {
+    let respostacerta1 = document.getElementById("alt12").checked
+
+    if (respostacerta1 == true) {
+        document.getElementById("resultado1").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado1").style.color = "green"
+        document.getElementById("situacao_atual").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado1").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado1").style.color = "red"
+        pontoAtual -1
+    } 
+}
+function pergunta2() {
+    let respostacerta2 = document.getElementById("alt21").checked
+
+    if (respostacerta2 == true) {
+        document.getElementById("resultado2").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado2").style.color = "green"
+        document.getElementById("situacao_atual2").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado2").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado2").style.color = "red"
+        pontoAtual -1
+    } 
+}
+function pergunta3() {
+    let respostacerta3 = document.getElementById("alt33").checked
+
+    if (respostacerta3 == true) {
+        document.getElementById("resultado3").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado3").style.color = "green"
+        document.getElementById("situacao_atual3").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado3").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado3").style.color = "red"
+        pontoAtual -1
+    } 
+}
+function pergunta4() {
+    let respostacerta4 = document.getElementById("alt43").checked
+
+    if (respostacerta4 == true) {
+        document.getElementById("resultado4").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado4").style.color = "green"
+        document.getElementById("situacao_atual4").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado4").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado4").style.color = "red"
+        pontoAtual -1
+    } 
+}
+function pergunta5() {
+    let respostacerta5 = document.getElementById("alt53").checked
+    
+    if (respostacerta5 == true) {
+        document.getElementById("resultado5").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado5").style.color = "green"
+        document.getElementById("situacao_atual5").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado5").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado5").style.color = "red"
+        pontoAtual -1
+    } 
+}
+function pergunta6() {
+    let respostacerta6 = document.getElementById("alt63").checked
+    
+    if (respostacerta6 == true) {
+        document.getElementById("resultado6").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado6").style.color = "green"
+        document.getElementById("situacao_atual6").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado6").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado6").style.color = "red"
+        pontoAtual -1
+    } 
+}
+function pergunta7() {
+    let respostacerta7 = document.getElementById("alt71").checked
+    
+    if (respostacerta7 == true) {
+        document.getElementById("resultado7").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado7").style.color = "green"
+        document.getElementById("situacao_atual7").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado7").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado7").style.color = "red"
+        pontoAtual -1
+    } 
+}
+function pergunta8() {
+    let respostacerta8 = document.getElementById("alt82").checked
+    
+    if (respostacerta8 == true) {
+        document.getElementById("resultado8").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado8").style.color = "green"
+        document.getElementById("situacao_atual8").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado8").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado8").style.color = "red"
+        pontoAtual -1
+    } 
+}
+function pergunta9() {
+    let respostacerta9 = document.getElementById("alt92").checked
+    
+    if (respostacerta9 == true) {
+        document.getElementById("resultado9").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado9").style.color = "green"
+        document.getElementById("situacao_atual9").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado9").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado9").style.color = "red"
+        pontoAtual -1
+    } 
+}
+function pergunta10() {
+    let respostacerta10 = document.getElementById("alt101").checked
+    
+    if (respostacerta10 == true) {
+        document.getElementById("resultado10").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado10").style.color = "green"
+        document.getElementById("situacao_atual10").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado10").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado10").style.color = "red"
+        pontoAtual -1
+    } 
+}
+function pergunta11() {
+    let respostacerta11 = document.getElementById("alt112").checked
+    
+    if (respostacerta11 == true) {
+        document.getElementById("resultado11").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado11").style.color = "green"
+        document.getElementById("situacao_atual11").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado11").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado11").style.color = "red"
+        pontoAtual -1
+    } 
+}
+function pergunta12() {
+    let respostacerta12 = document.getElementById("alt114").checked
+    
+    if (respostacerta12 == true) {
+        document.getElementById("resultado12").textContent = "Parabéns, você acertou essa questão!"
+        document.getElementById("resultado12").style.color = "green"
+        document.getElementById("situacao_atual12").textContent = pontoAtual + "/12"
+        pontoAtual = pontoAtual + 1
+    }else{
+        document.getElementById("resultado12").textContent = "Que pena, você errou essa questão!"
+        document.getElementById("resultado12").style.color = "red"
+        pontoAtual -1
+    } 
+}
+
+function ReiniciaJogo() {
+    window.location.reload()
+}
+function voltarinicio() {
+    window.scrollTo(0,0)
+}
